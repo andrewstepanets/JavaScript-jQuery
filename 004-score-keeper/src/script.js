0a4babcbe666939e6668ecdad34acdfaf5bda5c2
Andrii Stepanets <ovrass@gmail.com>
2020-08-30 23:59:00 +0300

    Finished example app

--- 004-score-keeper/src/script.js
+++ 004-score-keeper/src/script.js
@@ -4,11 +4,30 @@ const p2Display = document.querySelector('#p2Display');
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 const resetButton = document.querySelector('#resetButton');
+const numInput = document.querySelector('input[type=number]');
 
 let p1Score = 0;
 let p2Score = 0;
 let gameOver = false;
 let winScore = 5;
+let winningScoreDisplay = document.querySelector('p span');
+
+const reset = () => {
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = p1Score;
+    p2Display.textContent = p2Score;
+    p1Display.classList.remove('text-success');
+    p2Display.classList.remove('text-success');
+    gameOver = false;
+    resetButton.disabled = true;
+};
+
+numInput.addEventListener('change', () => {
+    winningScoreDisplay.textContent = numInput.value;
+    winScore = Number(numInput.value);
+    reset();
+});
 
 p1Button.addEventListener('click', () => {
     if(!gameOver){
@@ -21,6 +40,7 @@ p1Button.addEventListener('click', () => {
         p1Display.textContent = p1Score;
     }
 });
+
 p2Button.addEventListener('click', () => {
     if(!gameOver){
         p2Score++;
@@ -33,12 +53,7 @@ p2Button.addEventListener('click', () => {
     }
 });
 resetButton.addEventListener('click', () => {
-    p1Score = 0;
-    p2Score = 0;
-    p1Display.textContent = p1Score;
-    p2Display.textContent = p2Score;
-    p1Display.classList.remove('text-success');
-    p2Display.classList.remove('text-success');
-    gameOver = false;
-    resetButton.disabled = true;
+    reset();
 });
+
+
