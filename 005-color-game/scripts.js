f025fad8254f15e26786ad29c470c0732388ff05
Andrii Stepanets <ovrass@gmail.com>
2020-08-31 19:49:18 +0300

    Finished color Picker Game

--- 005-color-game/scripts.js
+++ 005-color-game/scripts.js
@@ -1,35 +1,47 @@
-const colors = [
-    'rgb(255, 0, 0)',
-    'rgb(255, 255, 0)',
-    'rgb(0, 255, 0)',
-    'rgb(0, 255, 255)',
-    'rgb(0, 0, 255)',
-    'rgb(255, 0, 255)',
-];
-
-// function helps 
-const pickColor = () => {
-     const random = Math.floor(Math.random() * colors.length);
-    return colors[random];
+const randomColor = () => {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    
+    return `rgb(${red}, ${green}, ${blue})`;
 }
 
+const generateRandomColor = num => {
+    const arr = [];
+    // add random colors to array
+    for (let index = 0; index < num; index++) {
+        // get random color and push into array
+        arr.push(randomColor());
+    }
+    return arr;
+};
+
+const colors = generateRandomColor(6);
+
+// function helps 
+const pickColor = (arr) => {
+     const random = Math.floor(Math.random() * arr.length);
+    return arr[random];
+}
+
+const header = document.querySelector('.header');
 const squares = document.querySelectorAll('.square');
-const pickedColor = pickColor();
+const pickedColor = pickColor(colors);
 const colorDisplay = document.querySelector('.colorDisplay');
 const message = document.querySelector('#message');
+const resetColorButton = document.querySelector('#resetColorButton');
 
 colorDisplay.textContent = pickedColor;
-
 
 
 const handleClick = event => {
     const { backgroundColor } = event.target.style;
     if(backgroundColor === pickedColor) {
         message.textContent = 'Color Correct!';
-        
         squares.forEach((square, color) => {
             square.style.backgroundColor = pickedColor;
         });
+        header.style.background = pickedColor;
     } else {
         event.target.style.backgroundColor = '#232323';
         message.textContent = 'Try Again!';
@@ -42,4 +54,5 @@ squares.forEach((square, idx) => {
     // add Event Listeners
     square.addEventListener('click', handleClick);
 });
+
 
